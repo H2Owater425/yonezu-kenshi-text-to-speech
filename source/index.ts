f9dbad6adbed7193bf93e8e6300297dfeb083cc4
H2Owater425 <h2o@dhmo.kr>
2025-06-15 05:52:04 +0900

    feat: Make volume preserved
    fix: Fix rendering order

--- source/index.ts
+++ source/index.ts
@@ -11,6 +11,7 @@ import { sleep, getImage, getListItem } from './utility';
 	const signatures: HTMLElement | null = document.getElementById("signatures");
 	const images: HTMLImageElement[] = [getImage("up"), getImage("down")];
 	const audioManager: AudioManager<number> = new AudioManager<number>(CODES);
+	const previousVolume: number = Number.parseInt(localStorage.getItem("volume") as string);
 	let index: number = 1;
 	let isPlaying: boolean = false;
 
@@ -18,10 +19,6 @@ import { sleep, getImage, getListItem } from './utility';
 		alert("Elements must exist");
 
 		return;
-	}
-
-	for(let i: number = 0; i < SIGNATURES["length"]; i++) {
-		signatures.appendChild(getListItem("<" + SIGNATURES[i] + ">", text));
 	}
 
 	function setImage(i: number): void {
@@ -32,100 +29,131 @@ import { sleep, getImage, getListItem } from './utility';
 		return;
 	}
 
-	audioManager.initialize()
-	.then(function (): void {
-		stop.addEventListener("click", function (): void {
-			isPlaying = false;
-			audioManager.stop();
+	if(!Number.isNaN(previousVolume) && previousVolume >= 0 && previousVolume <= 100) {
+		volume["valueAsNumber"] = previousVolume;
+		audioManager.setGain(previousVolume / 100);
+	}
 
-			return;
-		});
+	for(let i: number = 0; i < SIGNATURES["length"]; i++) {
+		signatures.appendChild(getListItem("<" + SIGNATURES[i] + ">", text));
+	}
 
-		clear.addEventListener("click", function (): void {
-			text["value"] = "";
-			text.focus();
+	start.setAttribute("disabled", "");
 
-			return;
-		});
-
-		volume.addEventListener("input", function (): void {
-			audioManager.setGain(volume["valueAsNumber"] / 100);
-
-			return;
-		});
-
-		const spaceAudioBuffer: AudioBuffer = audioManager.getBuffer(12288) as AudioBuffer;
-
-		start.addEventListener("click", function (): void {
-			if(!isPlaying) {
-				isPlaying = true;
-
-				const buffers: AudioBuffer[] = [];
-
-				for(let i: number = 0; i < text["value"]["length"]; i++) {
-					const code: number = text["value"].charCodeAt(i);
-
-					if(code === 60) {
-						const start: number = i + 1;
-
-						for(let j: number = 0; j < SIGNATURES["length"]; j++) {
-							if(text["value"].startsWith(SIGNATURES[j] + ">", start)) {
-								i = start + SIGNATURES[j]["length"];
-
-								buffers.push(audioManager.getBuffer(-j) as AudioBuffer);
-
-								break;
+	requestAnimationFrame(function (): void {
+		requestAnimationFrame(function (): void {
+			audioManager.initialize()
+			.then(function (): void {
+				start.removeAttribute("disabled");
+		
+				stop.addEventListener("click", function (): void {
+					isPlaying = false;
+					audioManager.stop();
+		
+					return;
+				});
+		
+				clear.addEventListener("click", function (): void {
+					text["value"] = "";
+					text.focus();
+		
+					return;
+				});
+		
+				volume.addEventListener("input", function (): void {
+					localStorage.setItem("volume", volume["value"]);
+					audioManager.setGain(volume["valueAsNumber"] / 100);
+		
+					return;
+				});
+		
+				const spaceAudioBuffer: AudioBuffer = audioManager.getBuffer(12288) as AudioBuffer;
+		
+				start.addEventListener("click", function (): void {
+					if(!isPlaying) {
+						isPlaying = true;
+		
+						const buffers: AudioBuffer[] = [];
+		
+						for(let i: number = 0; i < text["value"]["length"]; i++) {
+							const code: number = text["value"].charCodeAt(i);
+		
+							if(code === 60) {
+								const start: number = i + 1;
+		
+								for(let j: number = 0; j < SIGNATURES["length"]; j++) {
+									if(text["value"].startsWith(SIGNATURES[j] + ">", start)) {
+										i = start + SIGNATURES[j]["length"];
+		
+										buffers.push(audioManager.getBuffer(-j) as AudioBuffer);
+		
+										break;
+									}
+								}
+		
+								if(start < i) {
+									continue;
+								}
+							}
+		
+							const longCode: number = code + (text["value"].charCodeAt(i + 1) << 4);
+		
+							if(audioManager.hasBuffer(longCode)) {
+								buffers.push(audioManager.getBuffer(longCode) as AudioBuffer);
+		
+								i += 1;
+							} else if(audioManager.hasBuffer(code)) {
+								buffers.push(audioManager.getBuffer(code) as AudioBuffer);
+							} else {
+								buffers.push(spaceAudioBuffer);
 							}
 						}
-
-						if(start < i) {
-							continue;
-						}
-					}
-
-					const longCode: number = code + (text["value"].charCodeAt(i + 1) << 4);
-
-					if(audioManager.hasBuffer(longCode)) {
-						buffers.push(audioManager.getBuffer(longCode) as AudioBuffer);
-
-						i += 1;
-					} else if(audioManager.hasBuffer(code)) {
-						buffers.push(audioManager.getBuffer(code) as AudioBuffer);
-					} else {
-						buffers.push(spaceAudioBuffer);
-					}
-				}
-
-				buffers.reduce(function (previous: Promise<void>, current: AudioBuffer): Promise<void> {
-					return previous.then(function (): Promise<void> {
-						if(!isPlaying) {
-							return Promise.resolve();
-						}
-
-						if(current !== spaceAudioBuffer) {
-							sleep(current["duration"] / 2)
-							.then(function (): void {
-								setImage(0);
+		
+						buffers.reduce(function (previous: Promise<void>, current: AudioBuffer): Promise<void> {
+							return previous.then(function (): Promise<void> {
+								if(!isPlaying) {
+									return Promise.resolve();
+								}
+		
+								if(current !== spaceAudioBuffer) {
+									sleep(current["duration"] / 2)
+									.then(function (): void {
+										setImage(0);
+		
+										return;
+									});
+								}
+								
+								return audioManager.play(current)
+								.then(function (): Promise<void> {
+									setImage(1);
+		
+									return sleep(5);
+								});
 							});
-						}
-						
-						return audioManager.play(current)
-						.then(function (): Promise<void> {
+						}, Promise.resolve())
+						.then(function (): void {
+							isPlaying = false;
+		
 							setImage(1);
-
-							return sleep(5);
-						});
-					});
-				}, Promise.resolve())
-				.then(function (): void {
-					isPlaying = false;
-
-					setImage(1);
-
+		
+							return;
+						})
+						.catch(alert);
+					}
+		
 					return;
-				})
-				.catch(alert);
-			}
+				});
+		
+				return;
+			})
+			.catch(alert);
+
+			return;
 		});
+
+		return;
 	});
+
+	return;
 })();
